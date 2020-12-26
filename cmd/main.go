b7195f0d0ac05b5ae2d698b66386c99a7d67865c
hatlonely <hatlonely@gmail.com>
2020-12-26 23:53:30 +0800

    update

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,9 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
@@ -33,6 +36,8 @@ type Options struct {
 	Grpc struct {
 		Port int
 	}
+
+	ExitTimeout time.Duration `dft:"10s"`
 
 	Mysql         cli.MySQLOptions
 	Elasticsearch cli.ElasticSearchOptions
@@ -84,26 +89,43 @@ func main() {
 	svc, err := service.NewAncientServiceWithOptions(mysqlCli, esCli, &options.Service)
 	Must(err)
 
-	rpcServer := grpc.NewServer(rpcx.GRPCUnaryInterceptor(grpcLog, rpcx.WithDefaultValidator()))
-	api.RegisterAncientServiceServer(rpcServer, svc)
+	grpcServer := grpc.NewServer(rpcx.GRPCUnaryInterceptor(grpcLog, rpcx.WithDefaultValidator()))
+	api.RegisterAncientServiceServer(grpcServer, svc)
 
 	go func() {
 		address, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%v", options.Grpc.Port))
 		Must(err)
-		Must(rpcServer.Serve(address))
+		Must(grpcServer.Serve(address))
 	}()
 
-	muxServer := runtime.NewServeMux(
+	mux := runtime.NewServeMux(
 		rpcx.MuxWithMetadata(),
 		rpcx.MuxWithIncomingHeaderMatcher(),
 		rpcx.MuxWithOutgoingHeaderMatcher(),
 		rpcx.MuxWithProtoErrorHandler(),
 	)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
 	Must(api.RegisterAncientServiceHandlerFromEndpoint(
-		ctx, muxServer, fmt.Sprintf("0.0.0.0:%v", options.Grpc.Port), []grpc.DialOption{grpc.WithInsecure()},
+		context.Background(), mux, fmt.Sprintf("0.0.0.0:%v", options.Grpc.Port), []grpc.DialOption{grpc.WithInsecure()},
 	))
 	infoLog.Info(options)
-	Must(http.ListenAndServe(fmt.Sprintf(":%v", options.Http.Port), handlers.CombinedLoggingHandler(os.Stdout, muxServer)))
+	Must(http.ListenAndServe(fmt.Sprintf(":%v", options.Http.Port), handlers.CombinedLoggingHandler(os.Stdout, mux)))
+
+	httpServer := http.Server{Addr: fmt.Sprintf(":%v", options.Http.Port), Handler: mux}
+	go func() {
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			infoLog.Warnf("httpServer.ListenAndServe, err: [%v]", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+	infoLog.Info("receive exit signal")
+	ctx, cancel := context.WithTimeout(context.Background(), options.ExitTimeout)
+	defer cancel()
+	if err := httpServer.Shutdown(ctx); err != nil {
+		infoLog.Warnf("httServer.Shutdown failed, err: [%v]", err)
+	}
+	grpcServer.Stop()
+	infoLog.Info("server exit properly")
 }
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/golang/protobuf v1.4.3
 	github.com/gorilla/handlers v1.5.1
 	github.com/grpc-ecosystem/grpc-gateway v1.15.2
-	github.com/hatlonely/go-kit v1.0.15
+	github.com/hatlonely/go-kit v1.0.16-0.20201223053551-eb3f4f4e8f9d
 	github.com/jinzhu/gorm v1.9.15
 	github.com/olivere/elastic/v7 v7.0.19
 	github.com/pkg/errors v0.9.1
